dcf5f8fbf2d15fa147fb8f6ae2cfecc0b17b3ff4
Technochips <thetechnochips@protonmail.com>
2020-02-23 16:30:21 +0100

    clips out lines out of fov

--- subsector.js
+++ subsector.js
@@ -19,17 +19,26 @@ class Subsector
 		var a = (angle-90)*(Math.PI/180);
 		var angcos = Math.cos(a);
 		var angsin = Math.sin(a);
-		var x = (ox)*angcos-(-oy)*angsin - (graphics.width/2);
-		var y = (ox)*angsin+(-oy)*angcos - (graphics.height/2);
+		var ax = (ox)*angcos-(-oy)*angsin; //stands for (yet) **A**nother (fucking) x (variable)
+		var ay = (ox)*angsin+(-oy)*angcos; //same for this, i ran out of variable names
+		var x = ax - (graphics.width/2);
+		var y = ay - (graphics.height/2);
 		for(var seg in this.segs)
 		{
 			var s = this.segs[seg].direction == 0 ? this.segs[seg].startvertex : this.segs[seg].endvertex;
 			var e = this.segs[seg].direction == 1 ? this.segs[seg].startvertex : this.segs[seg].endvertex;
-			graphics.drawLine(c,
-			Math.floor((s.x*angcos - -s.y*angsin)-x),
-			Math.floor((s.x*angsin + -s.y*angcos)-y),
-			Math.floor((e.x*angcos - -e.y*angsin)-x),
-			Math.floor((e.x*angsin + -e.y*angcos)-y));
+			var sx = s.x*angcos - -s.y*angsin;
+			var sy = s.x*angsin + -s.y*angcos;
+			var ex = e.x*angcos - -e.y*angsin;
+			var ey = e.x*angsin + -e.y*angcos;
+			var sa = Math.atan2(sx-ax,sy-ay)*180/Math.PI;
+			var ea = Math.atan2(ex-ax,ey-ay)*180/Math.PI;
+			if((sa < -135 || sa > 135) || (ea < -135 || ea > 135))
+				graphics.drawLine(c,
+				Math.floor(sx-x),
+				Math.floor(sy-y),
+				Math.floor(ex-x),
+				Math.floor(ey-y));
 		}
 		var s = this.segs[this.segs.length-1].endvertex;
 		var e = this.segs[0].startvertex;
